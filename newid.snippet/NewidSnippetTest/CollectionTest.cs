2e860cb14e138c201e2d0b6c1854f7971926dce6
Miroslav Bartl <18615545+czmirek@users.noreply.github.com>
2019-12-26 13:06:05 +0100

    Rewritten template based on andrewlock strongly typed id

--- newid.snippet/NewidSnippetTest/CollectionTest.cs
+++ newid.snippet/NewidSnippetTest/CollectionTest.cs
@@ -9,35 +9,33 @@ namespace NewidSnippetTest
         [Fact]
         public void SortTest()
         {
-            List<SomeId> list = new List<SomeId>();
-
-            list.Add((SomeId)"c");
-            list.Add((SomeId)"b");
-            list.Add((SomeId)"a");
-            list.Add((SomeId)null);
+            List<SomeId> list = new List<SomeId>
+            {
+                (SomeId)"c",
+                (SomeId)"b",
+                (SomeId)"a"
+            };
 
             list.Sort();
 
-            Assert.Null(list[0].Value);
-            Assert.Equal("a", list[1].Value);
-            Assert.Equal("b", list[2].Value);
-            Assert.Equal("c", list[3].Value);
+            Assert.Equal("a", list[0].Id);
+            Assert.Equal("b", list[1].Id);
+            Assert.Equal("c", list[2].Id);
         }
 
         [Fact]
         public void DictionaryTest()
         {
-            Dictionary<SomeId, string> dict = new Dictionary<SomeId, string>();
-
-            dict.Add((SomeId)"a", "1");
-            dict.Add((SomeId)"b", "2");
-            dict.Add((SomeId)"c", "3");
-            dict.Add((SomeId)null, "4");
+            Dictionary<SomeId, string> dict = new Dictionary<SomeId, string>
+            {
+                { (SomeId)"a", "1" },
+                { (SomeId)"b", "2" },
+                { (SomeId)"c", "3" }
+            };
 
             Assert.Equal("1", dict[(SomeId)"a"]);
             Assert.Equal("2", dict[(SomeId)"b"]);
             Assert.Equal("3", dict[(SomeId)"c"]);
-            Assert.Equal("4", dict[(SomeId)null]);
         }
     }
 }
